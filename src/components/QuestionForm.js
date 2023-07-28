90233b540d1018fe1858bc5e8209650ca7f44906
Felix-Okeyo <felixokeyo20@gmail.com>
2023-07-28 13:37:11 +0300

    Create and Read with no Update and Delete

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 function QuestionForm(props) {
+  const [questions, setQuestions] = useState([]); // eslint-disable-next-line
+
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,8 +21,35 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
-  }
+    const newQuestion = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4
+      ],
+    };
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newQuestion),
+    })
+    .then((res) => res.json())
+    .then((question) => {
+      setQuestions(Questions => [...Questions, question]);
+      setFormData({
+        prompt: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        answer4: "",
+        correctIndex: 0
+      });
+    })
+  };
 
   return (
     <section>
